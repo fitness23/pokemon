15cb128e568aae749550f9ff3e8c41f4b55b3a5d
fitness23 <ajfhoward@hotmail.com>
2022-05-11 08:21:18 +0100

    Update http.service.ts
    Putting in new error catching.

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable, map } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
+import { Observable, map, catchError, throwError } from 'rxjs';
 import { Filter } from "../interfaces/filters-interface";
 import { Item } from "../interfaces/items-interface";
 
@@ -8,27 +8,35 @@ import { Item } from "../interfaces/items-interface";
 export class HttpService {
 
     constructor(private http: HttpClient) { }
-
-
     
     getApi(){
         return "https://pokeapi.co/api/v2";
     }
 
     getTypes(): Observable<Filter[]> {
-        return this.http.get<Filter[]>(`${this.getApi()}/type`, { responseType: 'json' }).pipe(map((res: any) => res.results));
+        return this.http.get<Filter[]>(`${this.getApi()}/type`, { responseType: 'json' }).pipe(
+            catchError(err => { return this.errorHandler(err) }),
+            map((res: any) => res.results));
     }
 
     getAbilities(): Observable<Filter[]> {
-        return this.http.get<Filter[]>(`${this.getApi()}/ability`, { responseType: 'json' }).pipe(map((res: any) => res.results));
+        return this.http.get<Filter[]>(`${this.getApi()}/ability`, { responseType: 'json' }).pipe(
+            catchError(err => { return this.errorHandler(err) }),
+            map((res: any) => res.results));
     }
 
     getPokemonList(searchUrl: string): Observable<Item[]> {
-        return this.http.get<Item[]>(`${searchUrl}`, { responseType: 'json' }).pipe(map((res: any) => res.pokemon));
+        return this.http.get<Item[]>(`${searchUrl}`, { responseType: 'json' }).pipe(
+            catchError(err => { return this.errorHandler(err) }),
+            map((res: any) => res.pokemon));
     }
 
     getPokemonDetail(searchUrl: string): Observable<any> {
         return this.http.get(searchUrl, { responseType: 'json', observe: 'response' });
+    }
+
+    errorHandler(error: HttpErrorResponse) {
+        return throwError(error.message || "server error.");
     }
 
 }
